a485cde811c1df1d70527b5f5a03bf51da174e89
Igor Sobral <igorgmz1421@gmail.com>
2023-11-29 17:11:59 -0300

    reset

--- src/components/layout/home/index.jsx
+++ src/components/layout/home/index.jsx
@@ -11,22 +11,7 @@ export default function Home() {
   const [visibleInput, setVisibleInput] = useState(false);
   const [saldo, setSaldo] = useState();
   const [input, setInput] = useState();
-  const [options, setOptions] = useState({
-    title: "Finanças",
-  });
-  const [data, setData] = useState([
-    ["Linguagens", "Quantidade"],
-    ["Nubank", 100],
-    ["picpay", 80],
-    ["Bradesco", 50],
-  ]);
-  const [dataBar] = useState([
-    ["Linguagens", "Quantidade"],
-    ["Nubank", 100],
-    ["picpay", 80],
-    ["Bradesco", 50],
-  ]);
-
+  
   function handleClick() {
     setVisible(() => !visible);
   }
@@ -42,52 +27,7 @@ export default function Home() {
   return (
     <div className="container">
 
-      <section className="section-content">
-        <div className="section">
-          <FaUserCircle className="userProf" />
-        </div>
-
-        <div className="saldo-content">
-          <p className="saldo">
-            Saldo: R$ {visible ? saldo : "_____"}
-            {!visible && (
-              <AiFillEyeInvisible
-                title="Ver saldo"
-                onClick={handleClick}
-                className="eye"
-              />
-            )}
-            {visible && (
-              <AiFillEye
-                title="Esconder saldo"
-                onClick={handleClick}
-                className="eye"
-              />
-            )}
-          </p>{" "}
-          <p className="addS">
-            Adicionar saldo{" "}
-            <VscDiffAdded className="add" onClick={handleInput} />
-          </p>
-          {visibleInput && (
-           <TextField id="filled-basic" helperText="adicione o valor" label="Saldo" variant="filled"  placeholder="R$:"
-              onChange={(e) => setInput(e.target.value)}
-              value={input}/>
-             
-            
-          )}
-          {visibleInput && (
-            <Button label="Adicionar" onClick={addSaldo}></Button>
-          )}
-        </div>
-      </section>
-      <section className="section-home">
-        <div className="section-primary">
-          <label className="label"></label>
-          <div className="primary-content"></div>
-        </div>
-      
-      </section>
+    
     </div>
   );
 }
