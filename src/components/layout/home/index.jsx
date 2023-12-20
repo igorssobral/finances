6243db727dfd43ab206f49ac8342ad620789795d
Igor Sobral <igorgmz1421@gmail.com>
2023-12-19 21:47:19 -0300

    refactor section content

--- src/components/layout/home/index.jsx
+++ src/components/layout/home/index.jsx
@@ -3,15 +3,15 @@ import { useState } from "react";
 import Chart from "react-apexcharts";
 import Donut from "../chart";
 import { BiSolidBank } from "react-icons/bi";
-import Icon from "../../ui/icon";
-
+import PaidIcon from "@mui/icons-material/Paid";
+import { Content } from "../../ui/section-content/content";
 
 export default function Home() {
   const [visible, setVisible] = useState(true);
   const [visibleInput, setVisibleInput] = useState(false);
   const [saldo, setSaldo] = useState();
   const [input, setInput] = useState();
-  
+
   function handleClick() {
     setVisible(() => !visible);
   }
@@ -19,42 +19,42 @@ export default function Home() {
     setVisibleInput(() => !visibleInput);
   }
   function addSaldo() {
-    console.log("🚀 ~ file: index.jsx:40 ~ Home ~ input:", input)
+    console.log("🚀 ~ file: index.jsx:40 ~ Home ~ input:", input);
     setSaldo(input);
-    
   }
- 
 
-  
-
-  
   return (
     <div className="container">
       <section className="section-content">
-
         <div className="primary-content">
-          <div className="content">
-          <BiSolidBank/>
-          </div>
-          <div className="content"></div>
-          <div className="content"></div>
+          <Content
+            children={
+              <BiSolidBank
+                fontSize={"40px"}
+                style={{ color: "#007ffd", padding: "10px" }}
+              />
+            }
+          />
+          <Content
+            children={
+              <PaidIcon
+                fontSize={"large"}
+                style={{ color: "#007ffd", padding: "10px" }}
+              />
+            }
+          />
+          <Content />
         </div>
 
         <div className="aside">
           <div className="chart">
-          <Donut/>
+            <Donut />
           </div>
-
+          <div></div>
         </div>
 
-        <div className="table-content">
-
-        </div>
-
-
+        <div className="table-content"></div>
       </section>
-    
     </div>
   );
-
-  }
+}
