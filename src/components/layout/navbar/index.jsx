48a8bc6a0854db61ac9d616f919ff0cd2efee5a5
Igor Sobral <igorgmz1421@gmail.com>
2023-12-20 19:40:16 -0300

    remove badge

--- src/components/layout/navbar/index.jsx
+++ src/components/layout/navbar/index.jsx
@@ -9,24 +9,20 @@ import {
 import { CgProfile } from "react-icons/cg";
 import { useState } from "react";
 import { Badge } from "@mui/material";
-import MailIcon from '@mui/icons-material/Mail';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MailIcon from "@mui/icons-material/Mail";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 export default function Navbar() {
   return (
     <nav>
       <ul className="pages">
         <NavItems label="Visão Geral" class="navi" />
-        <NavItems label="Transações" class="navi"/>
-        <NavItems label="Metas" class="navi"/>
+        <NavItems label="Transações" class="navi" />
+        <NavItems label="Metas" class="navi" />
       </ul>
       <ul className="pages">
-         <NavItems label="" href="#" icon={<AccountCircleIcon fontSize="" />} />
-      <NavItems label="" href="#" icon={<Badge badgeContent={1} color="secondary">
-  <MailIcon color="white" />
-</Badge> } />
+        <NavItems label="" href="#" icon={<AccountCircleIcon fontSize="" />} />
       </ul>
-     
     </nav>
   );
 }
