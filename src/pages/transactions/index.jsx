66a7d4381738fb82343302a332e7615219775b89
Igor Sobral <igorgmz1421@gmail.com>
2023-12-20 22:47:20 -0300

    add: page transactions

---
+++ src/pages/transactions/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./styles.css"
+const Transactions = () => {
+  return (
+    <div className='container'>
+        <div className="card">
+            
+        </div>
+        <div className="card_2">
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Transactions;
