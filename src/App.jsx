0af122b5d0e2d80aa590ff03c377a998924879c1
Igor Sobral <igorgmz1421@gmail.com>
2023-12-20 22:47:39 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@
 import './App.css'
 import Navbar from './components/layout/navbar'
 import Home from './components/layout/home'
+import Transactions from './pages/transactions'
 
 function App() {
 
   return (
     <>
       <Navbar/>
-    
-      <Home/>
+    <Transactions/>
+      {/* <Home/> */}
     </>
   )
 }
