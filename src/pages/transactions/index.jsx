5e4da1590127c092fd866869fef5e88f05e0f604
Igor Sobral <igorgmz1421@gmail.com>
2023-12-26 20:34:39 -0300

    add: forms transactions

--- src/pages/transactions/index.jsx
+++ src/pages/transactions/index.jsx
@@ -6,20 +6,29 @@ import "./styles.css";
 const Transactions = () => {
   return (
     <div className="container">
-      <div className="card"></div>
-      <div className="card_2">
-        
-        <form action="">
-      <Input label="Transação" type="text"/>
-      <Input label="Valor gasto" type="number"/>
-      <Input label="Data" type="date"/>
-      <Button style={{height: "fit-content", margin: "20px 0", backgroundColor: "white"}}>Adicionar</Button>
 
-        </form>
-        <div>
-          dddsdd
-        </div>
+      <div className="card">
+        <h2>Adicionar Transação</h2>
+        <form action="">
+          <Input label="Transação" type="text" />
+          <Input label="Valor gasto" type="number" />
+          <Input  label="Data" type="date" />
+          
+        </form><Button
+          
+            style={{
+              height: "fit-content",
+              width: "80%",
+              margin: "20px 0",
+              backgroundColor: "#697eb9",
+              color: "#fff",
+
+            }}
+          >
+            Adicionar
+          </Button>
       </div>
+      <div className="card_2"></div>
     </div>
   );
 };
