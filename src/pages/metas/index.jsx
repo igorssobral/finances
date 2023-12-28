bfb7e341f8093e6c0244e46da2136592df6951ca
Igor Sobral <igorgmz1421@gmail.com>
2023-12-27 21:55:07 -0300

    add: layout page metas

---
+++ src/pages/metas/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+const Meta = () => {
+  return (
+    <section className="container">
+      <div className="card1"></div>
+      <div className="card2"></div>
+      <div className="card3"></div>
+    </section>
+  );
+};
+
+export default Meta;
