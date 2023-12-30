1963e6a77a395e7b6ac661d158b902f4576844a8
Igor Sobral <igorgmz1421@gmail.com>
2023-12-29 21:08:56 -0300

    add form metas

--- src/pages/metas/index.jsx
+++ src/pages/metas/index.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { Input } from "../../components/ui/input";
 import "./style.css";
 const Meta = () => {
   return (
     <section className="container">
       <div className="card1"></div>
       <div className="card2"></div>
-      <div className="card3"></div>
+      <div className="card3">
+        <form action="">
+          <Input label="Meta" width="200px" />
+          <Input label="Valor  poupado mensal" width="200px" type="number"/>
+          <Input label="Data limite" width="200px" type="date"/>
+        </form>
+      </div>
     </section>
   );
 };
