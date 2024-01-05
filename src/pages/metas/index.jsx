2903fc1220836be9b6be607a29fbb39a7f10b04d
Igor Sobral <igorgmz1421@gmail.com>
2024-01-04 22:18:34 -0300

    add chart column in page metas

--- src/pages/metas/index.jsx
+++ src/pages/metas/index.jsx
@@ -5,11 +5,12 @@ import DeleteIcon from '@mui/icons-material/Delete';
 
 import "./style.css";
 import { Button } from "@mui/material";
+import ChartBar from "../../components/layout/chart/ChartBar";
 const Meta = () => {
   return (
     <section className="container">
       <div className="card1"></div>
-      <div className="card2"></div>
+      <div className="card2"><ChartBar/></div>
       <div className="card3">
         <form className="form-metas "action="">
           <Input label="Meta" width="200px" />
