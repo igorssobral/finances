13cb2414f769060305277bcd70761a9a32b7ff36
Igor Sobral <igorgmz1421@gmail.com>
2024-01-06 12:08:11 -0300

    add table metas

--- src/pages/metas/index.jsx
+++ src/pages/metas/index.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Input } from "../../components/ui/input";
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
-
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import SearchIcon from '@mui/icons-material/Search';
+import { ListContent } from "../../components/layout/list";
 import "./style.css";
 import { Button } from "@mui/material";
 import ChartBar from "../../components/layout/chart/ChartBar";
@@ -12,7 +14,7 @@ const Meta = () => {
       <div className="card1"></div>
       <div className="card2"><ChartBar/></div>
       <div className="card3">
-        <form className="form-metas "action="">
+        {/* <form className="form-metas "action="">
           <Input label="Meta" width="200px" />
           <Input label="Valor  poupado mensal" width="200px" type="number"/>
           <Input label="Data limite" width="200px" type="date"/>
@@ -27,9 +29,33 @@ const Meta = () => {
         >
           Adicionar
         </Button>
-        </form>
-        
-       
+        </form> */}
+        <div className="addMeta">
+          
+          <Input  />
+          <Button style={{
+            height: "max-content",
+            marginTop: "4px",
+            width: "max-content",
+            gap: "10px",
+            backgroundColor: "#697eb9",
+            color: "#fff",
+          }}><SearchIcon/></Button>
+
+
+          <Button style={{
+            height: "max-content",
+            marginTop: "4px",
+            width: "max-content",
+            gap: "10px",
+            backgroundColor: "#697eb9",
+            color: "#fff",
+          }}><AddCircleOutlineIcon/> Adicionar Meta</Button>
+        </div>
+       <section className="lists-metas">
+          <ListContent name="Viagem" />
+
+       </section>
       </div>
     </section>
   );
