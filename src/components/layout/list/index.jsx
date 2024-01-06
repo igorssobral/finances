62c1a9734c645385d7d0cb5c69df67421c733754
Igor Sobral <igorgmz1421@gmail.com>
2024-01-06 12:07:58 -0300

    create table metas

---
+++ src/components/layout/list/index.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import './style.css'
+export const ListContent = (props) => {
+  return (
+    <div className='list-content'>
+       <table class="table">
+  <thead>
+    <tr>
+    <th>Meta</th>
+    <th>Valor a poupar</th>
+    <th>Ações</th>
+  </tr></thead>
+  <tbody>
+    <tr>
+      <td >Viagem</td>
+      <td >R$2.000</td>
+      <td >Ver Detalhes</td>
+    </tr>
+    <tr>
+      <td >Viagem</td>
+      <td >R$2.000</td>
+      <td >Ver Detalhes</td>
+    </tr>
+    <tr>
+      <td >Viagem</td>
+      <td >R$2.000</td>
+      <td >Ver Detalhes</td>
+    </tr>
+    <tr>
+      <td >Viagem</td>
+      <td >R$2.000</td>
+      <td >Ver Detalhes</td>
+    </tr>
+    <tr>
+      <td >Viagem</td>
+      <td >R$2.000</td>
+      <td >Ver Detalhes</td>
+    </tr>
+    <tr>
+      <td >Viagem</td>
+      <td >R$2.000</td>
+      <td >Ver Detalhes</td>
+    </tr>
+    <tr>
+      <td >Viagem</td>
+      <td >R$2.000</td>
+      <td >Ver Detalhes</td>
+    </tr>
+    <tr>
+      <td >Viagem</td>
+      <td >R$2.000</td>
+      <td >Ver Detalhes</td>
+    </tr>
+  
+    
+  
+  </tbody>
+</table>
+    </div>
+  )
+}
