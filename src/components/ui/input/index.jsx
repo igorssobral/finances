3b7fc8401bafd8712b1208d81eb4feac44717a34
Igor Sobral <igorgmz1421@gmail.com>
2024-01-07 18:21:44 -0300

    add placeHolder

--- src/components/ui/input/index.jsx
+++ src/components/ui/input/index.jsx
@@ -5,7 +5,7 @@ export const Input = (props) => {
   return (
       <div className='input-content'>
         <label className='label' htmlFor={props.label}>{props.label}</label>
-        <input id={props.label} className='input-form' type={props.type}  style={{width: props.width || "300px"}} onChange={props.onChange} min={0}/>
+        <input id={props.label} placeholder={props.placeholder} className='input-form' type={props.type}  style={{width: props.width || "300px"}} onChange={props.onChange} min={0}/>
     </div>
   )
 }
