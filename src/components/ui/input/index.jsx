74e4dfcdcad5de1d5424987a4fdce75cdf2cbb2f
Igor Sobral <igorgmz1421@gmail.com>
2024-01-09 21:37:51 -0300

    add event onchange

--- src/components/ui/input/index.jsx
+++ src/components/ui/input/index.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import "./styles.css"
+import React from "react";
+import "./styles.css";
 
 export const Input = (props) => {
   return (
-      <div className='input-content'>
-        <label className='label' htmlFor={props.label}>{props.label}</label>
-        <input id={props.label} placeholder={props.placeholder} className='input-form' type={props.type}  style={{width: props.width || "300px"}} onChange={props.onChange} min={0}/>
+    <div className="input-content">
+      <label className="label" htmlFor={props.label}>
+        {props.label}
+      </label>
+      <input
+        id={props.label}
+        placeholder={props.placeholder}
+        className="input-form"
+        type={props.type}
+        style={{ width: props.width || "300px" }}
+        onChange={props.onChange}
+        min={0}
+      />
     </div>
-  )
-}
+  );
+};
